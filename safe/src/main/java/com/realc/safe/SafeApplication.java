314504658ad5d575ad096f544f2dad73657fee7b
lke-bai <wsbf19920302>
2020-06-17 11:57:47 +0800

    用户中心

--- safe/src/main/java/com/realc/safe/SafeApplication.java
+++ safe/src/main/java/com/realc/safe/SafeApplication.java
@@ -1,18 +1,15 @@
 package com.realc.safe;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @EnableDiscoveryClient
 @SpringBootApplication
+@EnableTransactionManagement
+@MapperScan(value = "com.realc.safe.mapper")
 public class SafeApplication {
 
     public static void main(String[] args) {
--- safe/src/main/java/com/realc/safe/controller/SsoController.java
+++ safe/src/main/java/com/realc/safe/controller/SsoController.java
@@ -1,9 +1,12 @@
 package com.realc.safe.controller;
 
+import com.alibaba.fastjson.JSONObject;
+import com.realc.safe.mapper.LoginLogMapper;
+import com.realc.safe.mapper.UserDetailMapper;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author lke-bai
@@ -12,15 +15,31 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @Slf4j
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class SsoController {
 
-    @GetMapping(value = "/hello/{str}", produces = "application/json")
+    private UserDetailMapper UserDetailMapper;
+    private LoginLogMapper LoginLogMapper;
+
+
+
+    @GetMapping(value = "/hello/{str}", produces = "application/json;charset=UTF-8")
     public String hello(@PathVariable String str) {
         log.info("-----------收到消费者请求-----------");
         log.info("收到消费者传递的参数：" + str);
         String result = "我是服务提供者，见到你很高兴==>" + str;
         log.info("提供者返回结果：" + result);
         return result;
+    }
+
+    @ResponseBody
+    @PostMapping(value = "/login", produces = "application/json;charset=UTF-8")
+    public String login(@RequestBody JSONObject json){
+        String userName = json.getString("user_name");
+        String password = json.getString("password");
+
+        UserDetailMapper.login(userName,password);
+        return "";
     }
 
 }
